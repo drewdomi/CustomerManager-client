5e3a69c0890c7af9b80027f57734146c21d9a932
Andrew Domingues <drewdomi@proton.me>
2023-07-03 09:42:12 -0300

    fix: exported names

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CustomerRegister from "./components/CustomerRegister";
 import CustomerSearch from "./components/CustomerSearch";
 
-export function Router() {
-  return(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<CustomerRegister />} />
-      <Route path="/search" element={<CustomerSearch />} />
-      <Route path="*" element={<CustomerRegister />} />
-    </Routes>
-  </BrowserRouter>
-  )
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CustomerRegister />} />
+        <Route path="/search" element={<CustomerSearch />} />
+        <Route path="*" element={<CustomerRegister />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default Router;
