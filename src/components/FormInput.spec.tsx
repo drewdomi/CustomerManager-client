ad0d6ec6a34aa215bcff89593db47400a1188433
Andrew Domingues <drewdomi@proton.me>
2023-07-28 13:35:01 -0400

    fix: add testing config

---
+++ src/components/FormInput.spec.tsx
@@ -0,0 +1,8 @@
+//import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/';
+
+describe("FormInput", () => {
+  it("Should not input a not integer", () => {
+    expect(1 + 1).toBe(2)
+  })
+})
