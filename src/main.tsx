92611ebaf471bbcfb58443205f9f28662fdc8941
Andrew Domingues <drewdomi@proton.me>
2023-08-29 22:23:54 -0300

    feat: add React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryCLient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <QueryClientProvider client={queryCLient}>
+    <App />
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
