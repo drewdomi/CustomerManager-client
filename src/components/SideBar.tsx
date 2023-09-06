ea69cba056edda76fc6ea8869c91bf0b28db1511
Andrew Domingues <drewdomi@proton.me>
2023-09-06 09:54:34 -0300

    fix: icons on SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -138,11 +138,13 @@ export default function SideBar() {
         <List>
 
           <ListItem disablePadding>
-            <ListItemLink to="/" primary="Pesquisar Cliente" icon={<PersonAddAlt1Icon />} />
+            <ListItemLink to="/" primary="Pesquisar Cliente"
+              icon={<PersonSearchIcon />} />
           </ListItem>
 
           <ListItem disablePadding >
-            <ListItemLink to="/register" primary="Cadastrar Cliente" icon={<PersonSearchIcon />} />
+            <ListItemLink to="/register" primary="Cadastrar Cliente"
+              icon={<PersonAddAlt1Icon />} />
           </ListItem>
 
         </List>
